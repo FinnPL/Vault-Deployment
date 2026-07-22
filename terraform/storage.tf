2a4ce61093068faf8f4c4e125bdcb936c97a49ca
Finn Lippok <contact@lippok.eu>
2026-07-22 18:19:13 +0200

    feat(terraform): add object storage lifecycle policies and audit log rotation

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -13,3 +13,42 @@ resource "oci_objectstorage_bucket" "backups" {
     prevent_destroy = true
   }
 }
+
+resource "oci_identity_policy" "objectstorage_lifecycle" {
+  compartment_id = var.tenancy_ocid
+  name           = "objectstorage-lifecycle"
+  description    = "Allow the Object Storage service to run lifecycle policies"
+
+  statements = [
+    "Allow service objectstorage-${var.region} to manage object-family in compartment id ${var.compartment_ocid}",
+  ]
+}
+
+resource "oci_objectstorage_object_lifecycle_policy" "backups" {
+  namespace = data.oci_objectstorage_namespace.ns.namespace
+  bucket    = oci_objectstorage_bucket.backups.name
+
+  rules {
+    name        = "expire-snapshots-after-30d"
+    action      = "DELETE"
+    is_enabled  = true
+    target      = "objects"
+    time_amount = 30
+    time_unit   = "DAYS"
+
+    object_name_filter {
+      inclusion_prefixes = ["raft/"]
+    }
+  }
+
+  rules {
+    name        = "purge-old-versions-after-7d"
+    action      = "DELETE"
+    is_enabled  = true
+    target      = "object-versions"
+    time_amount = 7
+    time_unit   = "DAYS"
+  }
+
+  depends_on = [oci_identity_policy.objectstorage_lifecycle]
+}
