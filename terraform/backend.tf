ea6292c92cf1f7b3854b5a8c792ad0ab6f026108
Finn Lippok <contact@lippok.eu>
2026-07-22 15:34:29 +0200

    feat(terraform): enable path style access for S3 backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,6 +11,8 @@ terraform {
 
     use_lockfile = true
 
+    use_path_style = true
+
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
